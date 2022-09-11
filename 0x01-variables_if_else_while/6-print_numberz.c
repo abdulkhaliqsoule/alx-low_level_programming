35f20d9c8365176d6ef8f4198c88e10e54ca272a
Abdul Khaliq <abdulkhaliqsoule@gmail.com>
2022-09-11 08:39:40 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always return o
+ *
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (; a < 10; a++)
+		putchar('0' + a);
+	putchar('\n');
+	return (0);
+}
