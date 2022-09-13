5520b99cfde7a29946b5d09fa0325ebbc75292b2
Abdul Khaliq <abdulkhaliqsoule@gmail.com>
2022-09-13 23:40:32 +0000

    is lower

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -16,4 +16,10 @@ void print_alphabet(void);
  *
  */
 void print_alphabet_x10(void);
+/**
+ * _islower - prints if c is lower case
+ * @c: character to use
+ * Return: 1 or 0
+ */
+int _islower(int c);
 #endif
