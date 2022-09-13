dd90a6c3fef24f7a8290a1878ec321b01515a26d
Abdul Khaliq <abdulkhaliqsoule@gmail.com>
2022-09-13 23:44:38 +0000

    isalpha

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -22,4 +22,10 @@ void print_alphabet_x10(void);
  * Return: 1 or 0
  */
 int _islower(int c);
+/**
+ * _isalpha - prints if character is alphabet
+ * @c: character to use
+ * Return: 1 or 0
+ */
+int _isalpha(int c);
 #endif
