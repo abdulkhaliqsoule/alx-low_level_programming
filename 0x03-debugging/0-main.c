a037856103f194375ff6b805e50d016a39a62b63
Abdul Khaliq <abdulkhaliqsoule@gmail.com>
2022-09-15 20:28:18 +0000

    pos or neg

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,11 +7,11 @@
 
 int main(void)
 {
-        int i;
+	int i;
 
-        i = 98 % 2;
-        positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 	printf("\n");
 
-        return (0);
+	return (0);
 }
