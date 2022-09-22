179aa36c2ce95d82cf3e0671d039e32b99df28a4
Abdul Khaliq <abdulkhaliqsoule@gmail.com>
2022-09-22 21:49:31 +0000

    concat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * _strcat - appends the src string to the dest string
+ * @dest: destination of concat
+ * @src: source of array to concat
+ *
+ * Return: char value
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0'
+	return (dest);
+}
