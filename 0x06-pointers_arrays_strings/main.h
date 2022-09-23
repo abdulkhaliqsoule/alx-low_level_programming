13fcd87bc9716619df633bcbcc7435554323a380
Abdul Khaliq <abdulkhaliqsoule@gmail.com>
2022-09-23 00:11:26 +0000

    assignments

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -172,4 +172,52 @@ char *_strcat(char *dest, char *src);
  * Return: char value
  */
 char *_strncat(char *dest, char *src, int n);
+/**
+ * _strncpy - copies a string
+ * @dest: destination to copy
+ * @src: source to copy
+ * @n: amount of places to copy
+ * Return: value of dest
+ */
+char *_strncpy(char *dest, char *src, int n);
+/**
+ * _strcmp - string to compare
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: value of string
+ */
+int _strcmp(char *s1, char *s2);
+/**
+ * reverse_array - reverses an array
+ *
+ * @a: array to reverse
+ * @n: size of array
+ */
+void reverse_array(int *a, int n);
+/**
+ * string_toupper - uppers a lower
+ * @s: character to make upper
+ * Return: return value of upper
+ */
+char *string_toupper(char *s);
+/**
+ * _indexOf - returns boolean if special  character
+ * @a: character to return
+ * Return: true or false
+ */
+int _indexOf(char a);
+/**
+ * cap_string - capitalizes the string
+ * @s: string
+ * Return: the string capitalized
+ */
+char *cap_string(char *s);
+/**
+ * leet - leet
+ * @s: string
+ *
+ * Return: char value
+ */
+char *leet(char *s);
 #endif
