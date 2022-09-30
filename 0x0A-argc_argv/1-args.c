e9073f049b9590e45438754f6531c1bffef81255
Abdul Khaliq <abdulkhaliqsoule@gmail.com>
2022-09-30 15:50:02 +0000

    nargs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints its name followed by a new line.
+ * @argc: number of parameters in argv
+ * @argv: char array containing all the passed in parameters.
+ *
+ * Return: always return 0.
+ */
+int main(int argc, __attribute__((unused))char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
