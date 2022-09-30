35e40c59f75c69399dc28743f6bc277fe6f037b8
Abdul Khaliq <abdulkhaliqsoule@gmail.com>
2022-09-30 17:09:04 +0000

    multiply

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the result of multiplication.
+ * @argc: number of parameters in argv
+ * @argv: char array containing all the passed in parameters.
+ *
+ * Return: always return 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc <= 1)
+	{
+		printf("Error");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
+
