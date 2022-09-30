73c541ce34934ce7c2461e86b87c1a67b984e85a
Abdul Khaliq <abdulkhaliqsoule@gmail.com>
2022-09-30 17:12:21 +0000

    multiply

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,9 +9,9 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc <= 1)
+	if (argc <= 1 || argc > 3)
 	{
-		printf("Error");
+		printf("Error\n");
 		return (1);
 	}
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
