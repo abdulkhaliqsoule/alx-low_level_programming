2cfd141133a6596ac3f8299ed05a9cb6525fd35c
Abdul Khaliq <abdulkhaliqsoule@gmail.com>
2022-10-03 18:54:58 +0000

    check 1

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * create_array - create an array of chars.
+ * @size: size of array
+ * @c: char to initialize with
+ *
+ * Return: a pointer to the array or null if it fails.
+ */
+char *create_array(unsigned int size, char c);
+#endif
