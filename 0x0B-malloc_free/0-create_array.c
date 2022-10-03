806a4c968efdea41c060c881d6de15f4276db701
Abdul Khaliq <abdulkhaliqsoule@gmail.com>
2022-10-03 19:44:08 +0000

    malloc-0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,10 +12,14 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-       	char *ptr;
+	char *ptr;
 
 	ptr = malloc(sizeof(*ptr) * size);
 	if (size <= 0)
+	{
+		return (NULL);
+	}
+	if (ptr == NULL)
 	{
 		return (NULL);
 	}
