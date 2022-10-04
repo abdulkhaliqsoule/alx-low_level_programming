0eef121709eb084926582cbda0ef35fd41f07622
Abdul Khaliq <abdulkhaliqsoule@gmail.com>
2022-10-04 08:53:39 +0000

    concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,11 +15,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		*s1 = '\0';
+		s1 = "";
 	}
 	if (s2 == NULL)
 	{
-		*s2 = '\0';
+		s2 = "";
 	}
 	for (i = 0; s1[i] != '\0'; i++)
 	{}
