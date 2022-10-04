5da79ae5f0585e45c2f55570cb9f162f92c2d3a7
Abdul Khaliq <abdulkhaliqsoule@gmail.com>
2022-10-04 18:09:08 +0000

    free grid

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -37,4 +37,10 @@ char *str_concat(char *s1, char *s2);
  * Return: a pointer or NULL.
  */
 int **alloc_grid(int width, int height);
+/**
+ * free_grid  - frees up a grid
+ * @grid: grid to check
+ * @height: height of grid
+ */
+void free_grid(int **grid, int height);
 #endif
