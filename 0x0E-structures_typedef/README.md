9def23c61e7cd9772291957ef09f45ff4822963c
Abdul Khaliq <abdulkhaliqsoule@gmail.com>
2022-10-10 19:27:10 +0000

    dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,7 @@
+0x0E. C - Structures, typedef
+
+0. Poppy - Define a new type struct dog with the following elements:
+
+name, type = char *
+age, type = float
+owner, type = char *
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - struct for dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ *
+ * Description: contains age, name and owner variables for dog
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+/*
+ * typedef struct dog my_dog;
+ */
+#endif
