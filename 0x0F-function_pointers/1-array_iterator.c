80dc65b1fb7973ede9a274a959320ae029a57baa
Abdul Khaliq <abdulkhaliqsoule@gmail.com>
2022-10-12 18:41:21 +0000

    print

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,9 +12,10 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
+
 	if (array != NULL && action != NULL)
 		while (i < size)
-		{	
+		{
 			action(array[i]);
 			i++;
 		}
