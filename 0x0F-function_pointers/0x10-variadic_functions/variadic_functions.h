412762326fd555ad33052490b5c8700fdc2649c1
Abdul Khaliq <abdulkhaliqsoule@gmail.com>
2022-10-13 21:38:03 +0000

    0

--- 0x0F-function_pointers/0x10-variadic_functions/variadic_functions.h
+++ 0x0F-function_pointers/0x10-variadic_functions/variadic_functions.h
@@ -7,4 +7,12 @@
  * Return: sum of arguments or 0
  */
 int sum_them_all(const unsigned int n, ...);
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
 #endif
