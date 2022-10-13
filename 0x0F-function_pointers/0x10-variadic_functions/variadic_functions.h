92386cd76af9b25e58a8a890b23e517194b38b2c
Abdul Khaliq <abdulkhaliqsoule@gmail.com>
2022-10-13 21:34:56 +0000

    0

---
+++ 0x0F-function_pointers/0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+/**
+ * sum_them_all - returns the sum of all its parameters
+ * @n: number of arguments
+ *
+ * Return: sum of arguments or 0
+ */
+int sum_them_all(const unsigned int n, ...);
+#endif
