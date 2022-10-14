5af63e9cc96c9d3ff491fb8cf6204f4156630aea
Abdul Khaliq <abdulkhaliqsoule@gmail.com>
2022-10-14 05:07:33 +0000

    final

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -32,4 +32,19 @@ void print_numbers(const char *separator, const unsigned int n, ...);
  * Return: Nothing.
  */
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+int print_c(va_list a);
+int print_i(va_list a);
+int print_f(va_list a);
+int print_s(va_list a);
+/**
+ * struct op - struct for 3
+ * @c: character to check
+ * @f: function to point to
+ */
+typedef struct op
+{
+	char *c;
+	int (*f)(va_list a);
+} printer;
 #endif
