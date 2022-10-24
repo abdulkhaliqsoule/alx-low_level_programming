aab55675ff350b3f767b9b6cc56897c997501c44
Abdul Khaliq <abdulkhaliqsoule@gmail.com>
2022-10-24 18:03:46 +0000

    l

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -23,9 +23,9 @@ list_t *add_node(list_t **head, const char *str)
 	}
 	new->str = strdup(str);
 	new->len = i;
-	new->next = *head;
+	new->next = head;
 
-	*head = new;
+	head = new;
 
 	return (new);
 }
