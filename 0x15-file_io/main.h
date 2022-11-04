9f1c2efa3d9f73e49a71486f70a0b6503c613f54
Abdul Khaliq <abdulkhaliqsoule@gmail.com>
2022-11-04 01:43:17 -0700

    requiscat

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,4 +14,11 @@
  * Return: NULL or 0
  */
 ssize_t read_textfile(const char *filename, size_t letters);
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _putchar(char c);
