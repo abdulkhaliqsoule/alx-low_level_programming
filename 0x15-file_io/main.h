56217ab0c20325db6f39f08030b3ba3007336465
Abdul Khaliq <abdulkhaliqsoule@gmail.com>
2022-11-04 03:50:33 -0700

    append

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -30,4 +30,11 @@ int _putchar(char c);
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content);
+/**
+ * append_text_to_file - appends to file
+ * @filename: name of file
+ * @text_content: content of text
+ * Return: int value
+ */
+int append_text_to_file(const char *filename, char *text_content);
 #endif
