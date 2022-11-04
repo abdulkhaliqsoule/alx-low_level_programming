5eab344f862e932d0108a6809b20d68859fd6390
Abdul Khaliq <abdulkhaliqsoule@gmail.com>
2022-11-04 03:02:24 -0700

    create

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -22,4 +22,12 @@ ssize_t read_textfile(const char *filename, size_t letters);
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c);
+/**
+ * create_file - creates a file
+ * @filename: name of file to create
+ * @text_content:string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content);
 #endif
